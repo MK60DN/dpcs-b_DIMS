68874c697651082c61469a653d097792cb53a20c
MK60DN <97679857+MK60DN@users.noreply.github.com>
2025-04-08 08:41:46 +0800

    新增

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        // 你可以在这里添加自定义颜色
+      },
+      fontFamily: {
+        // 你可以在这里添加自定义字体
+      },
+      spacing: {
+        // 你可以在这里添加自定义间距
+      }
+    },
+  },
+  plugins: [],
+}
